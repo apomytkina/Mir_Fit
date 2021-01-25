bc2416397ab4fe874004c8042d3cef12b5856269
KSabiyat <smkurbanova@edu.hse.ru>
2021-01-25 21:03:42 +0300

    Initial commit

---
+++ src/test/java/com/mirfit/mirfit/MirFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirfit.mirfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

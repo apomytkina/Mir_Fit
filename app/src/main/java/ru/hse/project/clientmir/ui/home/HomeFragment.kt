1c39aed6110c8283c81db60bce9c63ef04039f19
AlexStanchenko <60795996+AlexStanchenko@users.noreply.github.com>
2021-02-03 22:09:51 +0300

    Create Project

---
+++ app/src/main/java/ru/hse/project/clientmir/ui/home/HomeFragment.kt
@@ -0,0 +1,24 @@
+package ru.hse.project.clientmir.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import ru.hse.project.clientmir.R
+
+class HomeFragment : Fragment() {
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        return root
+    }
+}

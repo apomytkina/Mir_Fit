bcdd0cac26a88fe091f4bb8e6aba2bcbb2c4dba7
AlexStanchenko <60795996+AlexStanchenko@users.noreply.github.com>
2021-03-12 02:07:17 +0300

    refactor names of profile settings elements

--- app/src/main/java/ru/hse/project/mirfit/ui/settings/SettingsFragment.kt
+++ app/src/main/java/ru/hse/project/mirfit/ui/settings/SettingsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -17,31 +16,26 @@ class SettingsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.set, container, false)
+        val root = inflater.inflate(R.layout.fragment_profile_settings, container, false)
 
 
-        val layContent = root.findViewById<ConstraintLayout>(R.id.setting_content)
+        val layContent = root.findViewById<ConstraintLayout>(R.id.prof_set_front_content)
         layContent.findViewById<ConstraintLayout>(R.id.btn_change_login_password)
             .setOnClickListener {
-// появление фрагмента по выбору
+                // появление фрагмента по выбору
                 // фрагмент смены логина, и пароля
 
             }
 
-        val layNonContent = root.findViewById<ConstraintLayout>(R.id.bottom_content)
+        val layNonContent = root.findViewById<ConstraintLayout>(R.id.prof_set_back_content)
         // настройка поведения нижнего экрана
         val bottomSheetBehavior = BottomSheetBehavior.from<View>(layNonContent)
         layContent.findViewById<ConstraintLayout>(R.id.btn_about_mir_fit).setOnClickListener {
-            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED)
         }
 
 
 
-
-
-
-
-
-        return root;
+        return root
     }
 }

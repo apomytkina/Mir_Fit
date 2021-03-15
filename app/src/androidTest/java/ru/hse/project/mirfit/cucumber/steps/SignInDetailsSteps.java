9f013da7831e7db04b11883721daf5231d930ce0
Polina-Polyanskaya <papolyanskaya@edu.hse.ru>
2021-03-15 13:54:26 +0300

    Sign_in features and steps were added

--- app/src/androidTest/java/ru/hse/project/mirfit/cucumber/steps/SignInDetailsSteps.java
+++ app/src/androidTest/java/ru/hse/project/mirfit/cucumber/steps/SignInDetailsSteps.java
@@ -1,22 +1,15 @@
 package ru.hse.project.mirfit.cucumber.steps;
 
 
-import android.os.SystemClock;
-
-import ru.hse.project.mirfit.R;
 import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import ru.hse.project.mirfit.ui.auth.AuthActivity;
+import ru.hse.project.mirfit.R;
 
-import androidx.test.rule.ActivityTestRule;
-
-
-import static androidx.test.espresso.Espresso.*;
-import static androidx.test.espresso.action.ViewActions.*;
-import static androidx.test.espresso.assertion.ViewAssertions.*;
-import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 
 public class SignInDetailsSteps {
@@ -32,31 +25,28 @@ public class SignInDetailsSteps {
         onView(withId(R.id.frag_sign_login)).perform(click());
     }
 
-    @And("^I enter valid login \"([^\"]*)\"$")
-    public void enterValidLogin(String login) {
-        onView(withId(R.id.frag_sign_login)).perform(typeText(login));
-    }
-
+    @And("^I enter login \"([^\"]*)\"$")
+    public void enterValidLogin(String login) { onView(withId(R.id.frag_sign_login)).perform(typeText(login)); }
 
     @And("^I click password field$")
     public void clickPasswordField() {
         onView(withId(R.id.frag_sign_password)).perform(click());
     }
 
-    @And("^I enter valid password \"([^\"]*)\"$")
-    public void enterValidPassword(String password) {
-        onView(withId(R.id.frag_sign_password)).perform(typeText(password));
-    }
+    @And("^I enter password \"([^\"]*)\"$")
+    public void enterValidPassword(String password) { onView(withId(R.id.frag_sign_password)).perform(typeText(password)); }
 
     @And("^I click button SIGN IN in authorize form$")
-    public void clickSignInAuthorizeForm() {
-        onView(withId(R.id.frag_sign_btn)).perform(click());
-    }
+    public void clickSignInAuthorizeForm() { onView(withId(R.id.frag_sign_btn)).perform(click()); }
 
     @Then("^I expect to see successful changing the screen$")
-    public void checkExistButtonAddCard() {
-        Utils.closeApp();
-    }
+    public void checkExistButtonAddCard() { Utils.closeApp(); }
 
+    @Then("^I expect to the same screen and notification with error message$")
+    public void i_expect_to_the_same_screen_and_notification_with_error_message()
+    {}
 
+    @Then("^I expect to the same screen and error in the field$")
+    public void i_expect_to_the_same_screen_and_notification_error_in_the_field()
+    {}
 }

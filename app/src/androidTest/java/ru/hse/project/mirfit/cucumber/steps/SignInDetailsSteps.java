7d47ce28e6b5f625f176603b5cc80eeecdacbc13
Polina-Polyanskaya <71229619+Polina-Polyanskaya@users.noreply.github.com>
2021-04-01 18:39:48 +0300

    Update SignInDetailsSteps.java

--- app/src/androidTest/java/ru/hse/project/mirfit/cucumber/steps/SignInDetailsSteps.java
+++ app/src/androidTest/java/ru/hse/project/mirfit/cucumber/steps/SignInDetailsSteps.java
@@ -48,9 +48,8 @@ public class SignInDetailsSteps {
     //Нажатие на кнопку вход после заполнения полей
     @And("^I click button SIGN IN in authorize form$")
     public void clickSignInAuthorizeForm() {
-        SystemClock.sleep(1000);
         onView(withId(R.id.frag_sign_btn)).perform(click());
-        SystemClock.sleep(3000);
+        SystemClock.sleep(1000);
     }
 
     //Проверка, что будет переход нас следущий экран

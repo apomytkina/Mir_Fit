3f3f22e351e18e248f8714710061129e5285feed
DESKTOP-5JJ58IH\User <shevkomn@gmail.com>
2021-04-01 16:47:38 +0300

    Add card varification by core

--- src/main/java/com/mirfit/mirfit/services/CardServiceImpl.java
+++ src/main/java/com/mirfit/mirfit/services/CardServiceImpl.java
@@ -1,17 +1,27 @@
 package com.mirfit.mirfit.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mirfit.mirfit.models.CardDto;
 import com.mirfit.mirfit.models.GetBonusesResponse;
 import com.mirfit.mirfit.models.GetCardsResponse;
 import com.mirfit.mirfit.repositories.CardRepository;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
 @Service
 public class CardServiceImpl implements CardService {
+    private static ObjectMapper mapper = new ObjectMapper();
     private final CardRepository cardRepository;
+    private final String BASE_URL = "https://mir-acquirer.herokuapp.com/info/card-number-exists";
+    private static final OkHttpClient client = new OkHttpClient();
 
     @Autowired
     public CardServiceImpl(CardRepository cardRepository) {
@@ -68,9 +78,33 @@ public class CardServiceImpl implements CardService {
 
     @Override
     public String addCard(UUID id, CardDto card) {
+        // Check if user is a client of bank
+        if (!validateCardByCore(card.getNumber())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Card not found");
+        }
+
         if (id != null)
             return cardRepository.addCard(id, card);
         else
             return "Id cannot be null.";
+    }
+
+    private boolean validateCardByCore(String cardNumber) {
+        try {
+            Request request = new Request.Builder()
+                    .url(BASE_URL + "?cardNumber=" + cardNumber)
+                    .build();
+
+            Response response = client.newCall(request).execute();
+
+            if (response.isSuccessful()) {
+               return  Boolean.parseBoolean(response.body().string());
+            } else {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error connecting to core");
+            }
+
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error connecting to core");
+        }
     }
 }
